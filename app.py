7ab374a48afb861fb5776e557b1c579e300706b7
mei-ldm <98782783+mei-ldm@users.noreply.github.com>
2022-01-31 16:19:40 -0600

    Add files via upload
    try to update the to-do item based on whether it is a completed item.

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+import sys
+
+app = Flask(__name__) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://meiliuxy@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICAITONS'] = False 
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, default=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+#db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  error = False
+  body = {}
+  try: 
+    description = request.get_json()['description']
+    todo = Todo(description=description, completed=False)
+    db.session.add(todo)
+    db.session.commit()
+    body ['id'] = todo.id 
+    body ['completed'] = todo.completed
+    body ['description'] = todo.description
+  except: 
+    error = True
+    db.session.rollback() 
+    print(sys.exc_info())
+  finally: 
+    db.session.close()
+  if error: 
+  	abort (400)
+  if not error: 
+    return jsonify(body)
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try: 
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
+ 
+
+@app.route('/')
+def index(): 
+  return render_template('index.html', todos=Todo.query.order_by('id').all())
+
+
+
+if __name__ =='__main__':
+	app.run(host="0.0.0.0")
